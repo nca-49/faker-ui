7acd96fdc61e57fef12f18aa050b3d569908160b
nca49 <ruddy@gmail.com>
2024-12-13 14:18:23 +0300

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
